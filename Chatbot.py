729b541b92e4b6319f7503880d0b37798d63bd2d
HyperPenetrator <hrishikeshdutta2006@gmail.com>
2025-06-05 14:37:00 +0530

    Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-API_KEY = "AIzaSyB-rcBOUodQkJmi3Or1IBZlK9VByL9y3uY"
+API_KEY = "Use your own API Key"     #Api key can be obtained from: https://aistudio.google.com/apikey edit "Use your own API Key" and enter the Api key to run the program ;)
 genai.configure(api_key=API_KEY)
 
 model = genai.GenerativeModel("gemini-2.0-flash")
